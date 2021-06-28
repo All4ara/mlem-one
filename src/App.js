024787410c37a5d67ee9034dc478f86e0181e3b6
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-06-28 19:05:32 -0400

    set up tinder swipe functions with a responsive design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from "./components/Header"
 import MlemCards from "./components/MlemCards"
-
+import SwipeButtons from "./components/SwipeButtons"
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <MlemCards /> 
 
       {/* SwipeButtons */}
+      <SwipeButtons />
 
     </div>
   );
