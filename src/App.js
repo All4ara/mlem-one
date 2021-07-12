6bbac9c0f1cbaf445e16e8cdf33d432eb08d936d
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-07-12 10:16:30 -0400

    set up react-router-dom for chat feature's between users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
+import React from "react";
 import './App.css';
 import Header from "./components/Header"
 import MlemCards from "./components/MlemCards"
 import SwipeButtons from "./components/SwipeButtons"
+import { BrowserRouter  as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
 
-      {/* Header */}
+    {/* Header */}
       <Header />
 
-      {/* MlemCards */}
-      <MlemCards /> 
+      <Router>
+        <Switch>
 
-      {/* SwipeButtons */}
-      <SwipeButtons />
+          <Route path="/chat">
 
+            {/* Chats Screen */}
+
+            {/* Individual Chats Screen */}
+
+          </Route>
+
+          <Route path="/">
+
+            {/* MlemCards */}
+            <MlemCards /> 
+
+            {/* SwipeButtons */}
+            <SwipeButtons />
+
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+        {/* Header */}
+        <Header />
+
+        {/* MlemCards */}
+        <MlemCards /> 
+
+        {/* SwipeButtons */}
+        <SwipeButtons />
+
+        {/* Chats Screen */}
+
+        {/* Individual Chats Screen */}
+        
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import Logo from "../assets/mlem-logo.png"
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 function Header() {
     return (
